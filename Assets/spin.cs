e44b1a0574f8d5cdfbc0d4825b8a223e74b9a973
Max Wei <max.wei@alumni.ubc.ca>
2019-02-15 06:39:14 -0800

    simple coin

---
+++ Assets/spin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    private Transform transform;
+    public float speed = -3f; //degrees per update
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = this.GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward * speed);
+    }
+}
